ad464652095e65246484ea94c33ad35fcf5bb710
NobodyExceptHim <70288263+NobodyExceptHim@users.noreply.github.com>
2021-01-12 12:41:50 +0100

    Indexing

--- Settings.jsx
+++ Settings.jsx
@@ -1,16 +1,16 @@
 const { React } = require('powercord/webpack');
-const { TextInput, SwitchItem, Category } = require('powercord/components/settings');
+const { TextInput, SwitchItem, Category, Button } = require('powercord/components/settings');
 
-
-
-module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
+module.exports = ({ getSetting, updateSetting, toggleSetting, main }) => (
     
     <div>
         <TextInput
         note='ID used to jump start'
         defaultValue={getSetting('IDset', '1111')}
         required={true}
-        onChange={val => updateSetting('IDset', val)}
+        onChange={val => {updateSetting('IDset', val); 
+            main.ete();
+        }}
         >
             Client ID
         </TextInput>
@@ -18,13 +18,13 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
             name='Text Settings'
             description={<span>Settings for the discord RPC text.</span>}
             opened={getSetting('textSettings', false)}
-            onChange={() => toggleSetting('textSettings')}
+            onChange={() => {toggleSetting('textSettings'); main.ete();}}
         >
             <TextInput
                 note='First line of the Discord RPC.'
                 defaultValue={getSetting('lineone', 'Hello!')}
                 required={true}
-                onChange={val => updateSetting('lineone', val)}
+                onChange={val => {updateSetting('lineone', val); main.ete();}}
             >
                 Line One
             </TextInput>
@@ -32,7 +32,7 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
                 note='Second line of the Discord RPC.'
                 defaultValue={getSetting('linetwo')}
                 required={false}
-                onChange={val => updateSetting('linetwo', val)}
+                onChange={val => {updateSetting('linetwo', val); main.ete();}}
             >
                 Line Two
             </TextInput>
@@ -41,13 +41,13 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
         name='Image Settings'
         description={<span>Settings for the discord RPC images.</span>}
         opened={getSetting('imageSettings', false)}
-        onChange={() => toggleSetting('imageSettings')}
+        onChange={() => {toggleSetting('imageSettings'); main.ete();}}
         >
             <TextInput
                 note='Large image shown on the Discord RPC.'
                 defaultValue={getSetting('largeimage', 'null')}
                 required={false}
-                onChange={val => updateSetting('largeimage', val)}
+                onChange={val => {updateSetting('largeimage', val); main.ete();}}
             >
                 Large Image
             </TextInput>
@@ -55,7 +55,7 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
                 note='Text shown when hovering over the large image.'
                 defaultValue={getSetting('largeimagetext', 'null')}
                 required={false}
-                onChange={val => updateSetting('largeimagetext', val)}
+                onChange={val => {updateSetting('largeimagetext', val); main.ete();}}
             >
                 Large Image Text
             </TextInput>
@@ -63,7 +63,7 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
                 note='Small image shown on the Discord RPC.'
                 defaultValue={getSetting('smallimage', 'null')}
                 required={false}
-                onChange={val => updateSetting('smallimage', val)}
+                onChange={val => {updateSetting('smallimage', val); main.ete();}}
             >
                 Small Image
             </TextInput>
@@ -71,7 +71,7 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
                 note='Text shown when hovering over the small image.'
                 defaultValue={getSetting('smallimagetext', 'null')}
                 required={false}
-                onChange={val => updateSetting('smallimagetext', val)}
+                onChange={val => {updateSetting('smallimagetext', val); main.ete();}}
             >
                 Small Image Text
             </TextInput>
@@ -79,14 +79,14 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
         <SwitchItem
         note='Timestamp for the Discord RPC.'
         value={getSetting('timestamp', false)}
-        onChange={() => toggleSetting('timestamp')}
+        onChange={() => {toggleSetting('timestamp'); main.ete();}}
         >
             Timestamp
         </SwitchItem>
         <SwitchItem
         note='Enable/Disable the commands for controlling the Discord RPC'
         value={getSetting('commandsEnabled', false)}
-        onChange={()=> toggleSetting('commandsEnabled')}
+        onChange={()=> {toggleSetting('commandsEnabled'); main.ete();}}
         >
             Commands
         </SwitchItem>
--- index.js
+++ index.js
@@ -1,28 +1,80 @@
 const { Plugin } = require('powercord/entities');
-
+const { React, getModule } = require('powercord/webpack');
+const { remote } = require('electron');
 const Settings = require('./Settings');
+
+const RPC = require("discord-rpc")
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+
+
 
 module.exports = class DiscordRPC extends Plugin {
     startPlugin() {
         powercord.api.settings.registerSettings(this.entityID, {
             category: this.entityID,
             label: 'DiscordRPC',
-            render: Settings
+            render: (props) => React.createElement(Settings, {
+                ...props,
+                main: this
+              })
         });
 
-        powercord.api.commands.registerCommand({
-            command: 'discordrpc',
-            description: 'Set this thing man',
-            usage: 'hi',
-            executor: this.handleCommand.bind(this),
-            autocomplete: this.handleAutocomplete.bind(this)
+        rpc.login({
+            clientId: this.settings.get('IDset', '790602350612054026')
         });
+
+        const timestamp = new Date().getTime();
+
+        const setActivity = async () => {
+            switch (this.settings.get('timestamp')) {
+                case true: {
+                    rpc.setActivity({
+                        details: this.settings.get('lineone', 'Hello!'),
+                        state: this.settings.get('linetwo', 'I am cool.'),
+                        startTimestamp: timestamp,
+                        largeImageKey: this.settings.get('largeimage', null),
+                        largeImageText: this.settings.get('largeimagetext', null),
+                        smallImageKey: this.settings.get('smallimage', null),
+                        smallImageText: this.settings.get('smallimagetext', null)
+                    });
+                    break;
+                }
+                case false: {
+                    rpc.setActivity({
+                        details: this.settings.get('lineone', 'Hello!'),
+                        state: this.settings.get('linetwo', 'I am cool.'),
+                        largeImageKey: this.settings.get('largeimage', null),
+                        largeImageText: this.settings.get('largeimagetext', null),
+                        smallImageKey: this.settings.get('smallimage', null),
+                        smallImageText: this.settings.get('smallimagetext', null)
+                    });
+                    break;
+                }
+            }
+        }
+
+        this.mikoma = async () => {
+
+        }
+
+
+        rpc.on("ready", () => {
+            setActivity();
+
+            this.ete = async () => {
+                setActivity();
+            }
+        });
+        
     }
 
     pluginWillUnload () {
         powercord.api.settings.unregisterSettings(this.entityID);
         powercord.api.commands.unregisterCommand('discordrpc');
     }
-
+    
 
 };
